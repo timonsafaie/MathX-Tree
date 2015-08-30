37b8fb5f1b8668f0083d32664452a13d3b2fc811
Wenhua Zhao <whzhao@gmail.com>
2015-08-30 12:30:44 -0700

    removed multiChar deletion

--- src/cursor.js
+++ src/cursor.js
@@ -69,9 +69,6 @@ extend(Cursor, Elem, function(_) {
         if (prev instanceof Mrow && !prev.highlighted) {
             prev.highlight();
             return;
-        } else if (prev.info.multiChar) {
-            this.expandAgg(prev);
-            prev = this.prev;
         }
         prev.putCursorBefore(this);
         prev.remove();
@@ -84,9 +81,6 @@ extend(Cursor, Elem, function(_) {
         if (next instanceof Mrow && !next.highlighted) {
             next.highlight();
             return;
-        } else if (next.info.multiChar) {
-            this.expandAgg(next, true);
-            next = this.next;
         }
         next.putCursorBefore(this);
         next.remove();
