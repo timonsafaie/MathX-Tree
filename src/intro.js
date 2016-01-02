687d3a2f8d3f4d42b0028da851a3fd70c24bfeba
Wenhua Zhao <whzhao@gmail.com>
2016-01-02 01:00:23 -0800

    fixed fraction delimiter and subsup css offset

--- src/intro.js
+++ src/intro.js
@@ -1,5 +1,5 @@
 var entrees = { 0: null };
 var _mxCF = (function() {
-var version = 'basic';
+var version = 'full';
 if (typeof MathXVersionLevel != "undefined") version = MathXVersionLevel;
 var version_number  = '1.0';        // Software Version
