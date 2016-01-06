fa0cc13812e26b4470511a61ac3089ae8ebe46b4
Jason McInerney <jason@demonstranda.com>
2016-01-05 19:56:16 -0800

    Still adding in changes: expose transalate functions

--- src/intro.js
+++ src/intro.js
@@ -1,5 +1,4 @@
 var _mxCF = (function() {
-(function() {
 var version = 'full';
 if (typeof MathXVersionLevel != "undefined") version = MathXVersionLevel;
 var version_number  = '1.0';        // Software Version
